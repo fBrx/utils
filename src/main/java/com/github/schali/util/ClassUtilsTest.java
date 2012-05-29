f4398d70de3cba156198f8973bd420d64ec77623
schali <admin@florian-m.net>
2012-05-29 21:55:18 +0200

    added AppendableObjectOutputStream

---
+++ src/main/java/com/github/schali/util/ClassUtilsTest.java
@@ -0,0 +1,14 @@
+package com.github.schali.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClassUtilsTest {
+
+	@Test
+	public void testWhich() {
+		fail("Not yet implemented");
+	}
+
+}
