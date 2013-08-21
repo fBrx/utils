ec037b805cac0f51d70145046f8caf2f1034a837
fBrx <admin@florian-m.net>
2013-08-21 23:29:43 +0200

    formatted code

--- src/test/java/com/github/schali/util/ClassUtilsTest.java
+++ src/test/java/com/github/schali/util/ClassUtilsTest.java
@@ -10,7 +10,7 @@ public class ClassUtilsTest {
 	public void testWhichNull() {
 		Assert.assertNull(ClassUtils.which(null));
 	}
-	
+
 	@Test
 	public void testWhichThis() {
 		Assert.assertNotNull(ClassUtils.which(this.getClass()));
